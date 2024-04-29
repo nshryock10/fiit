00591502a862c978738263ad32007a7854b9ad48
Nick Shryock <n.shryock10@gmail.com>
2024-04-28 21:21:55 -0400

    added vercel fiels

--- src/App.js
+++ src/App.js
@@ -45,8 +45,8 @@ function App() {
         const dev_URL = 'http://localhost:3000/';
         const prod_URL = 'https://fiit-8a6ab7670425.herokuapp.com';
         const prod_URL2 = 'https://app.tryfiit.com';
-        const prod_URL3 = 'https://aiworkout-ruby.vercel.app';
-        const socket = io(dev_URL);//Add final socket server URL
+        const prod_URL3 = 'https://fiit-mu.vercel.app';
+        const socket = io(prod_URL3);//Add final socket server URL
         socket.on('connect', () => console.log(socket.id));
         socket.on('connect_error', (err)=>{
             console.log('error in socket')
